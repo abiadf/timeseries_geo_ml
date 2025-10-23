e82fb6a2af47e46f43cfa1c11b671d62d33daf80
abiadf <abiadfm@gmail.com>
2025-10-23 22:34:58 +0200

    about to start fixing ts2vec to NOT pool anymore (currently using mean pool)

--- src/other_encoders/ts2vec_encoder.py
+++ src/other_encoders/ts2vec_encoder.py
@@ -2,7 +2,7 @@
 import numpy as np
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader, TensorDataset
+# from torch.utils.data import DataLoader, TensorDataset
 
 from ts2vec import TS2Vec # library
 
