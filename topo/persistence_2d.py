283773299f0935407ec815d9c9959a5e1bcedd8c
abiadf <abiadfm@gmail.com>
2026-06-09 00:12:48 +0200

    sped up streaming scenario down to ~0s :)

--- topo/persistence_2d.py
+++ topo/persistence_2d.py
@@ -5,7 +5,6 @@ import torch
 import numpy as np
 from numba import njit, prange
 from dataclasses import dataclass
-import heapq
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
